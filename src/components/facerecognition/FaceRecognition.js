78b46fd71ac71f39dd1725f6d30814bb20027a5d
TheFlyer1983 <theflyer1983@gmail.com>
2021-04-06 17:34:28 +0100

    Adding multiple face detection

--- src/components/facerecognition/FaceRecognition.js
+++ src/components/facerecognition/FaceRecognition.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({imageUrl, box}) => {
-  return(
-    <div className='center ma'>
-      <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto' />
-        <div className='bounding_box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+const FaceRecognition = ({ imageUrl, boxes }) => {
+  return (
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img id="inputimage" alt="" src={imageUrl} width="500px" height="auto" />
+        {boxes.map((box) => {
+          return (
+            <div
+              key={box.topRow}
+              className="bounding_box"
+              style={{
+                top: box.topRow,
+                right: box.rightCol,
+                bottom: box.bottomRow,
+                left: box.leftCol,
+              }}></div>
+          );
+        })}
       </div>
     </div>
   );
-}
+};
 
 export default FaceRecognition;
